18b520ad93ba7e04afae33d5df8b77755e5ac176
Alexandre Bouchard <bouchard@stat.ubc.ca>
2024-11-09 14:14:04 -0800

    Generalize to multiple csvs

--- src/combine.jl
+++ src/combine.jl
@@ -1,16 +1,15 @@
 """
-    combine_csvs(source; file_delim = "___", file_assign = "=", csv_parsing_args...)
+    combine_csvs(input_dir, output_dir; file_delim = "___", file_assign = "=", csv_parsing_args...)
 
 Reads in a collection of CSV files and perform a join based on file name information. 
 
-The `source` argument can be either of type `Vector{String}`, to provide a list of paths 
-to CSVs, or a single `String` which is then interpreted as a directory from which all the 
-`.csv` files in it will be read.
+The `input_dir` argument is the path to a directory from which all the 
+`.csv` files in it will be read. The `output_dir` is a directory where the output will be written to.
 
-We assume the file names following the format:
+We assume the file location and names conform the following format:
 ```
-key1=value11___key2=value21.csv 
-key1=value12___key2=value22.csv 
+input_dir/key1=value11___key2=value21/my_csv.csv 
+input_dir/key1=value12___key2=value22/my_csv.csv
 ...
 ```
 where we assume `keyi` does not contain the string `=` and that
@@ -24,53 +23,72 @@ value12___value22
 ``` 
 which is parsed via `CSV.read(...; delim = "___")``. 
 
-Each CSV is read with `CSV.read(paths[i], DataFrame, csv_parsing_args...)`, while 
+The CSVs are read with `CSV.read(paths[i], DataFrame, csv_parsing_args...)`, while 
 checking the column names match. We also check that the keys in file names are 
 disjoint to the key used inside the CSVs. 
 
 For each row in a given file, the key value pairs implied by its name are appended to the left.
+The output is written to output_dir/my_csv.csv
+
+If the input directories contain not just my_csv.csv but my_csv1.csv, my_csv2.csv, etc, the process 
+is repeated. We do not assume my_csv1.csv and my_csv2.csv have the same columns. 
 
 The default choice of `___` and `=` can be overridden with `file_delim` and `file_assign` respectively. 
 """
-function combine_csvs(source; file_delim = "___", file_assign = "=", csv_parsing_args...)
-    paths = get_paths(source)
-    @assert paths isa Vector "We need deterministic order on the paths."
-    if isempty(paths)
-        return DataFrame() 
-    end
-    index = index_data_frame(paths, file_delim, file_assign)
-    result = nothing
-    prev_names =  nothing
-    for i in eachindex(paths)
-        current = CSV.read(paths[i], DataFrame; csv_parsing_args...)
-        if i == 1
-            prev_names = names(current)
-            @assert isdisjoint(names(index), prev_names) "Header and csv keys expected disjoint but got: $(names(index)), $(names(current))"
-            result = vcat(similar(index, 0), similar(current, 0), cols = :union)
-        else
-            @assert names(current) == prev_names "All csv should have the same keys but got: $(names(current)), $prev_names"
+function combine_csvs(input_dir, output_dir; file_delim = "___", file_assign = "=", csv_parsing_args...)
+    mkpath(output_dir)
+    plan = organize_paths(input_dir, file_assign)
+    
+    for key in keys(plan) 
+        paths = plan[key]
+
+        index = index_data_frame(paths, file_delim, file_assign)
+        result = nothing
+        prev_names =  nothing
+        for i in eachindex(paths)
+            # note: this could be done in constant memory, but not a huge 
+            # priority since downstream scripts will need to load in memory the merged one anyways
+            current = CSV.read("$input_dir/$(paths[i])/$key", DataFrame; csv_parsing_args...)
+            if i == 1
+                prev_names = names(current)
+                @assert isdisjoint(names(index), prev_names) "Header and csv keys expected disjoint but got: $(names(index)), $(names(current))"
+                result = vcat(similar(index, 0), similar(current, 0), cols = :union)
+            else
+                @assert names(current) == prev_names "All csv should have the same keys but got: $(names(current)), $prev_names"
+            end
+            index_row = Tuple(index[i, :])
+            for r in eachrow(current)
+                data_row = Tuple(r) 
+                push!(result, (index_row..., data_row...))
+            end
         end
-        index_row = Tuple(index[i, :])
-        for r in eachrow(current)
-            data_row = Tuple(r) 
-            push!(result, (index_row..., data_row...))
+
+        CSV.write("$output_dir/$key", result)
+    end
+
+    return nothing
+end
+
+function organize_paths(directory::String, file_assign) 
+    result = Dict{String, Vector{String}}() # name.csv -> ["key1=value11___key2=value12", "key1=value22___key2=value22", ...]
+    for sub_dir_name in filter(x -> contains(x, file_assign), readdir(directory))
+        cur_sub_dir = "$directory/$sub_dir_name" 
+        for csv_file in filter(x -> endswith(x, ".csv"), readdir(cur_sub_dir)) 
+            list = get_or_push!(result, csv_file)
+            push!(list, sub_dir_name)
         end
     end
     return result
 end
 
-function file_name(key_values; file_delim = "___", file_assign = "=")
-    for (k, v) in pairs(key_values)
-        @assert !contains(file_delim, string(k))
-        @assert !contains(file_delim, string(v)) 
-        @assert !contains(file_assign, string(k))
+function get_or_push!(dict, key) 
+    if haskey(dict, key) 
+        return dict[key]
     end
-    strings = map(kv -> "$(kv[1])$file_assign$(kv[2])", collect(pairs(key_values)))
-    return join(strings, file_delim) * ".csv"
+    result = Vector{String}()
+    dict[key] = result 
+    return result
 end
-
-get_paths(source::Vector) = source 
-get_paths(directory::String) = map(x -> "$directory/$x", filter(x -> endswith(x, ".csv"), readdir(directory)))
 
 function index_data_frame(paths::Vector{String}, file_delim::String, file_assign::String)
     ref = parse_key_value_csv(paths[1], file_delim, file_assign)
@@ -92,7 +110,6 @@ end
 
 function parse_key_value_csv(path::String, file_delim::String, file_assign::String)
     str = basename(path)
-    str = remove_csv_suffix(str)
     return map(split(str, file_delim)) do entry 
         kv = split(entry, file_assign)
         @assert length(kv) ≥ 2 "Expected LHS$(file_delim)RHS but found: $entry"
@@ -101,9 +118,4 @@ function parse_key_value_csv(path::String, file_delim::String, file_assign::Stri
         end
         return kv
     end
-end
-
-function remove_csv_suffix(str)
-    @assert endswith(str, ".csv") "key_value csv should end in .csv"
-    return str[1:(end-4)]
 end
